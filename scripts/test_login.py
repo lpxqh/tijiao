a7d1112f7d8447b36d7744f2ab70b6a043d88688
lpxqh <1229290791@qq.com>
2019-06-18 14:43:13 +0800

    再次修改

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -11,3 +11,15 @@ class TestLogin:
 
     def test_login4(self):
         assert 1
+
+    def test_login5(self):
+        assert 1
+
+    def test_login6(self):
+        assert 1
+
+    def test_login7(self):
+        assert 1
+
+
+
